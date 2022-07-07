c25ac1730803c691b2105a27eabc1045297eb8db
Ori Baram <obrm770@gmail.com>
2022-07-07 20:15:31 +0300

    fixed typo

--- src/components/Folder.js
+++ src/components/Folder.js
@@ -4,20 +4,20 @@ import { FaRegFolderOpen } from "react-icons/fa";
 import { FaRegFileAlt } from "react-icons/fa";
 
 const Folder = ({ explorer }) => {
-  const [expanded, setExpanded] = useState(false);
+  const [isExpanded, setIsExpanded] = useState(false);
 
   if (explorer.isFolder) {
     return (
       <div>
-        {expanded ? <FaRegFolderOpen /> : <FaRegFolder />}{" "}
+        {isExpanded ? <FaRegFolderOpen /> : <FaRegFolder />}{" "}
         <span
-          onClick={() => setExpanded(!expanded)}
+          onClick={() => setIsExpanded(!isExpanded)}
           style={{ fontWeight: "bold", cursor: "pointer" }}
         >
           {explorer.name}
         </span>
         <br />
-        {expanded && (
+        {isExpanded && (
           <div style={{ paddingLeft: 15 }}>
             {explorer.items.map((exp) => {
               return <Folder explorer={exp} key={exp.name} />;
